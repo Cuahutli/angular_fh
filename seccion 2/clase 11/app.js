bea080a763199681268c142dec23d40c25df787f
Cuahutli Miguel Ulloa Robles <ing.cuahutliulloa@gmail.com>
2017-09-08 17:50:32 -0600

    clase 11 introducción a los tipos de datos

---
+++ seccion 2/clase 11/app.js
@@ -0,0 +1,19 @@
+var nombre;
+var numero;
+var boleano;
+var hoy;
+var cualquiera;
+nombre = "Piter";
+numero = 123;
+boleano = true;
+hoy = new Date("2017-10-12");
+cualquiera = { "nombre": "juan" };
+console.log(hoy);
+var spiderman = {
+    nombre: "Piter",
+    edad: 30,
+};
+spiderman = {
+    nombre: "Juan",
+    edad: 32,
+};
---
+++ seccion 2/clase 11/app.ts
@@ -0,0 +1,23 @@
+let nombre:string;
+let numero:number;
+let boleano:boolean;
+let hoy:Date;
+let cualquiera:any;
+
+nombre = "Piter";
+numero = 123;
+boleano = true;
+hoy = new Date("2017-10-12");
+cualquiera = {"nombre": "juan"};
+
+console.log(hoy);
+
+let spiderman = {
+    nombre: "Piter",
+    edad: 30,
+}
+
+spiderman = {
+    nombre: "Juan",
+    edad: 32,
+}
