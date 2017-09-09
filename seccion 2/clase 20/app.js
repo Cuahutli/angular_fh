385e41aa454a9df4b7618b59e1eba99e3505a46c
Cuahutli Miguel Ulloa Robles <ing.cuahutliulloa@gmail.com>
2017-09-09 12:50:40 -0600

    clase 20 definiendo una clase en TS

---
+++ seccion 2/clase 20/app.js
@@ -0,0 +1,8 @@
+var Avenger = (function () {
+    function Avenger() {
+        this.nombre = "Antman";
+    }
+    return Avenger;
+}());
+var antman = new Avenger();
+console.log(antman);
---
+++ seccion 2/clase 20/app.ts
@@ -0,0 +1,14 @@
+class Avenger {
+    nombre:string = "Antman";
+    equipo:string;
+    nombreReal:string;
+
+    puedePelear:boolean;
+    peleasGanadas:number;
+
+}
+
+
+let antman:Avenger = new Avenger();
+
+console.log(antman);
