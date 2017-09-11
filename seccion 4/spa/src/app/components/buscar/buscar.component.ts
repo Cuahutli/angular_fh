a5a470af0b45040c3820e8ba7c799fa377102ce1
Cuahutli Miguel Ulloa Robles <ing.cuahutliulloa@gmail.com>
2017-09-11 15:36:54 -0600

    clase 61 plus mostando mensaje cuando no hay resultados

--- seccion 4/spa/src/app/components/buscar/buscar.component.ts
+++ seccion 4/spa/src/app/components/buscar/buscar.component.ts
@@ -9,14 +9,15 @@ import { HeroesService } from "../../services/heroes.service";
 })
 export class BuscarComponent implements OnInit{
   heroes:any[] = [];
+  termino:string;
 
   constructor( private _activatedRoute:ActivatedRoute, 
                private _heroesService:HeroesService) {
    }
    ngOnInit(){
       this._activatedRoute.params.subscribe( params => {
-        // console.log(params['termino']);
-        this.heroes = this._heroesService.buscarHeroes(params['termino']);
+        this.termino = params['termino'];
+        this.heroes = this._heroesService.buscarHeroes(this.termino);
         // console.log(this.heroes);
       })
    }
