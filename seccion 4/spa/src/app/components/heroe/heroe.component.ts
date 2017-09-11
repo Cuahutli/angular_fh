2a55030f66523ee5d0d7d38fba5ba119545d8be2
Cuahutli Miguel Ulloa Robles <ing.cuahutliulloa@gmail.com>
2017-09-11 14:10:26 -0600

    clase 54 recibiendo parametros  por url - ActivatedRoute

--- seccion 4/spa/src/app/components/heroe/heroe.component.ts
+++ seccion 4/spa/src/app/components/heroe/heroe.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
+import { HeroesService } from "../../services/heroes.service";
 
 @Component({
   selector: 'app-heroe',
   templateUrl: './heroe.component.html',
   styles: []
 })
-export class HeroeComponent implements OnInit {
+export class HeroeComponent {
 
-  constructor() { }
+  heroe:any = {};
 
-  ngOnInit() {
-  }
+  constructor(private _activatedRoute:ActivatedRoute,
+              private _heroesService:HeroesService) {
+      this._activatedRoute.params.subscribe( params => {
+        console.log(params['id']);
+        this.heroe = this._heroesService.getHeroe(params['id']);
+      })
+   }
+
 
 }
