8b640f205df5b37281ea2f9a64e7eb144e7998c0
Cuahutli Miguel Ulloa Robles <ing.cuahutliulloa@gmail.com>
2017-09-18 15:18:24 -0600

    clase 109 localstorage almacenando la información

--- seccion 7/deseos/src/app/services/lista-deseos.service.ts
+++ seccion 7/deseos/src/app/services/lista-deseos.service.ts
@@ -7,16 +7,22 @@ export class ListaDeseosService {
     listas:Lista[] = [];
     
     constructor() { 
-        let lista1 = new Lista("Compras de Supermercado");
-        let lista2 = new Lista("Juegos que deseo");
-        let lista3 = new Lista("Cosas de la Universidad");
+        this.cargarData();
+    }
 
-        this.listas.push(lista1);
-        this.listas.push(lista2);
-        this.listas.push(lista3);
-        console.log("servicio inicializado");
+    actualizarData(){
+        localStorage.setItem("data", JSON.stringify(this.listas))
+    }
 
+    cargarData(){
+        if(localStorage.getItem("data")){
+            this.listas = JSON.parse(localStorage.getItem("data"));
+        }
+    }
 
+    agregarLista( lista:Lista){
+        this.listas.push(lista);
+        this.actualizarData();
     }
 
 }
