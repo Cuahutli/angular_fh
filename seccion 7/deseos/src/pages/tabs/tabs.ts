975acdf3a671ce771ea30e5d7ded22dff2dc4899
Cuahutli Miguel Ulloa Robles <ing.cuahutliulloa@gmail.com>
2017-09-18 13:31:13 -0600

    agregando primer versión ionic con cambios

---
+++ seccion 7/deseos/src/pages/tabs/tabs.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+import { AboutPage } from '../about/about';
+import { ContactPage } from '../contact/contact';
+import { HomePage } from '../home/home';
+
+import { PendientesComponent } from "../pendientes/pendientes.component";
+import { TerminadosComponent } from "../terminados/terminados.component";
+
+@Component({
+  templateUrl: 'tabs.html'
+})
+export class TabsPage {
+
+  // tab1Root = HomePage;
+  tab1Root = PendientesComponent;
+  tab2Root = TerminadosComponent;
+  // tab3Root = ContactPage;
+
+  constructor() {
+
+  }
+}
