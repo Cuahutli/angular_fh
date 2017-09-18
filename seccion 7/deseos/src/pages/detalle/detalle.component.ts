59a31d3acc6d09a1ab6025d403477ae8ca4bbf8b
Cuahutli Miguel Ulloa Robles <ing.cuahutliulloa@gmail.com>
2017-09-18 16:46:03 -0600

    clase 113 confirmación de eliminación

--- seccion 7/deseos/src/pages/detalle/detalle.component.ts
+++ seccion 7/deseos/src/pages/detalle/detalle.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { NavController, NavParams } from "ionic-angular";
+import { NavController, NavParams, AlertController } from "ionic-angular";
+import { Lista, ListaItem } from "../../app/classes/index";
 
+import { ListaDeseosService } from "../../app/services/lista-deseos.service";
 @Component({
     selector: 'app-detalle',
     templateUrl: 'detalle.component.html'
@@ -8,10 +10,12 @@ import { NavController, NavParams } from "ionic-angular";
 export class DetalleComponent implements OnInit {
 
     idx:number;
-    lista:any;
+    lista:Lista;
 
     constructor(public navCtrl:NavController,
-                public navParams:NavParams) {
+                public navParams:NavParams,
+                public alertCtrl:AlertController,
+                public _listaDeseos:ListaDeseosService) {
 
         this.idx = this.navParams.get("idx");
         this.lista = this.navParams.get("lista");
@@ -19,6 +23,46 @@ export class DetalleComponent implements OnInit {
 
     ngOnInit() { 
 
+    }
+
+    actualizar(item:ListaItem){
+        item.completado = !item.completado;
+        let todosMarcados = true;
+        for(let item of this.lista.items){
+            if (!item.completado){
+                todosMarcados = false;
+                break;
+            }
+        }
+
+        this.lista.terminada = todosMarcados;
+
+        this._listaDeseos.actualizarData();
+    }
+
+    borrarItem(){
+        let confirm = this.alertCtrl.create({
+            title: this.lista.nombre,
+            message: '¿Está Seguro que desea eliminar la lista?',
+            buttons: [
+            //   {
+            //     text: 'Cancelar',
+            //     // handler: () => {
+            //     //   console.log('Cancelar clicked');
+            //     // }
+            //   },
+            'Cancelar',
+              {
+                text: 'Eliminar',
+                handler: () => {
+                //   console.log('Agree clicked');
+                    this._listaDeseos.eliminarLista(this.idx);
+                    this.navCtrl.pop();
+                }
+              }
+            ]
+          });
+          confirm.present();
     }
 
 }
