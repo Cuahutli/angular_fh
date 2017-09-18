9655c80276bd64f748239ac3393f6942d5b68f72
Cuahutli Miguel Ulloa Robles <ing.cuahutliulloa@gmail.com>
2017-09-18 15:07:57 -0600

    clase 108 agregando al servicio, alertas y retornando a la pantalla principal

--- seccion 7/deseos/src/pages/agregar/agregar.component.ts
+++ seccion 7/deseos/src/pages/agregar/agregar.component.ts
@@ -1,18 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController, NavController } from "ionic-angular";
 
 import { Lista, ListaItem } from "../../app/classes/index";
+
+import { ListaDeseosService } from "../../app/services/lista-deseos.service";
 
 @Component({
     selector: 'app-agregar',
     templateUrl: 'agregar.component.html'
 })
 export class AgregarComponent implements OnInit {
-    nombreLista:string;
+    nombreLista:string = "";
     nombreItem:string;
 
     items:ListaItem[] = [];
 
-    constructor() { }
+    constructor( public alertCtrl:AlertController,
+                public navCtrl:NavController,
+                public _listaDeseos:ListaDeseosService) { }
 
     ngOnInit() { 
 
@@ -33,6 +38,22 @@ export class AgregarComponent implements OnInit {
         // console.log("borraremos este index:" + idx);
         let borrado = this.items.splice(idx,1);
         console.log(borrado[0].nombre);
+    }
+
+    guardarLista(){
+        if (this.nombreLista.length == 0){
+                let alert = this.alertCtrl.create({
+                  title: 'Nombre de la Lista',
+                  subTitle: 'El nombre de la lista es necesario',
+                  buttons: ['OK']
+                });
+                alert.present();
+                return;
+        }
+        let lista = new Lista(this.nombreLista);
+        lista.items = this.items;
+        this._listaDeseos.listas.push(lista);
+        this.navCtrl.pop();
     }
 
 }
