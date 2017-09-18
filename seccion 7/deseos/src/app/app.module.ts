3f75fdd7e74be776085d77ef200b48019196d46b
Cuahutli Miguel Ulloa Robles <ing.cuahutliulloa@gmail.com>
2017-09-18 13:33:18 -0600

    quitando comentarios y cosas innecesarias

--- seccion 7/deseos/src/app/app.module.ts
+++ seccion 7/deseos/src/app/app.module.ts
@@ -3,9 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-// import { AboutPage } from '../pages/about/about';
-// import { ContactPage } from '../pages/contact/contact';
-// import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
@@ -18,9 +15,6 @@ import { TerminadosComponent } from "../pages/terminados/terminados.component";
 @NgModule({
   declarations: [
     MyApp,
-    // AboutPage,
-    // ContactPage,
-    // HomePage,
     TabsPage,
     PendientesComponent,
     TerminadosComponent
@@ -32,9 +26,6 @@ import { TerminadosComponent } from "../pages/terminados/terminados.component";
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    // AboutPage,
-    // ContactPage,
-    // HomePage,
     TabsPage,
     PendientesComponent,
     TerminadosComponent
--- seccion 7/deseos/src/pages/tabs/tabs.ts
+++ seccion 7/deseos/src/pages/tabs/tabs.ts
@@ -1,9 +1,5 @@
 import { Component } from '@angular/core';
 
-import { AboutPage } from '../about/about';
-import { ContactPage } from '../contact/contact';
-import { HomePage } from '../home/home';
-
 import { PendientesComponent } from "../pendientes/pendientes.component";
 import { TerminadosComponent } from "../terminados/terminados.component";
 
@@ -12,10 +8,8 @@ import { TerminadosComponent } from "../terminados/terminados.component";
 })
 export class TabsPage {
 
-  // tab1Root = HomePage;
   tab1Root = PendientesComponent;
   tab2Root = TerminadosComponent;
-  // tab3Root = ContactPage;
 
   constructor() {
 
