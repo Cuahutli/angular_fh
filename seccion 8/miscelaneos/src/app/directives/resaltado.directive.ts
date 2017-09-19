0812a6412ef894a4f895cc0b09e2e2a35eac71cc
Cuahutli Miguel Ulloa Robles <ing.cuahutliulloa@gmail.com>
2017-09-19 17:25:26 -0600

    clase 124 directivas personalizadas

---
+++ seccion 8/miscelaneos/src/app/directives/resaltado.directive.ts
@@ -0,0 +1,30 @@
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
+
+@Directive({
+  selector: '[appResaltado]'
+})
+export class ResaltadoDirective {
+
+  constructor(private _el:ElementRef) { 
+    console.log("directiva llamada")
+    
+  }
+
+  @Input("appResaltado") nuevoColor:string;
+
+  @HostListener('mouseenter') 
+  mouseEntro(){
+    this.resaltar(this.nuevoColor || "yellow");
+  }
+
+  @HostListener('mouseleave') 
+  mouseSalio(){
+    this.resaltar(null);
+  }
+
+  private resaltar(color:string){
+    this._el.nativeElement.style.backgroundColor = color;
+  }
+
+
+}
