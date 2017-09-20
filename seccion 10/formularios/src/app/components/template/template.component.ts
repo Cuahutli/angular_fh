e2f92064a47ab98c8f47494ef29539bdcdb58bad
Cuahutli Miguel Ulloa Robles <ing.cuahutliulloa@gmail.com>
2017-09-20 14:56:02 -0600

    clase 149 obteniendo la información del formulario

--- seccion 10/formularios/src/app/components/template/template.component.ts
+++ seccion 10/formularios/src/app/components/template/template.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from "@angular/forms";
 
 @Component({
   selector: 'app-template',
@@ -7,13 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemplateComponent implements OnInit {
 
+  usuario:any = {
+    nombre:"Cuahutli",
+    apellido:"Ulloa",
+    email:""
+  }
+
   constructor() { }
 
   ngOnInit() {
   }
   
-  guardar(){
-    console.log("formulario posteado");
+  guardar(forma:NgForm){
+    console.log("ngForm:", forma);
+    console.log("Valor forma", forma.value);
+    console.log("Usuario", this.usuario);
   }
 
 }
