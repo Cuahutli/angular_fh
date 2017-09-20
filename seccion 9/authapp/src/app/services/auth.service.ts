a591073c41ed11ca7d10d92d76f267c980a2d836
Cuahutli Miguel Ulloa Robles <ing.cuahutliulloa@gmail.com>
2017-09-20 12:45:17 -0600

    obteniendo el perfil del usuario que inicio sesión

--- seccion 9/authapp/src/app/services/auth.service.ts
+++ seccion 9/authapp/src/app/services/auth.service.ts
@@ -12,8 +12,10 @@ export class AuthService {
     responseType: 'token id_token',
     audience: 'https://scit.auth0.com/userinfo',
     redirectUri: 'http://localhost:4200/callback',      
-    scope: 'openid'
+    scope: 'openid profile'
   });
+
+  public userProfile: any;
 
   constructor(public router: Router) {}
 
@@ -56,6 +58,21 @@ export class AuthService {
     // access token's expiry time
     const expiresAt = JSON.parse(localStorage.getItem('expires_at'));
     return new Date().getTime() < expiresAt;
+  }
+
+  public getProfile(cb): void {
+    const accessToken = localStorage.getItem('access_token');
+    if (!accessToken) {
+      throw new Error('Access token must exist to fetch profile');
+    }
+  
+    const self = this;
+    this.auth0.client.userInfo(accessToken, (err, profile) => {
+      if (profile) {
+        self.userProfile = profile;
+      }
+      cb(err, profile);
+    });
   }
 
 }
