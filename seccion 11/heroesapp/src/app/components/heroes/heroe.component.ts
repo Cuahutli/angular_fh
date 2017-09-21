4b3b1f56a2e9f0001077760baeeb42d8c901f94f
Cuahutli Miguel Ulloa Robles <ing.cuahutliulloa@gmail.com>
2017-09-21 13:57:59 -0600

    clase 173 HTTP-POST- creando un nuevo registro en firebase

--- seccion 11/heroesapp/src/app/components/heroes/heroe.component.ts
+++ seccion 11/heroesapp/src/app/components/heroes/heroe.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from "@angular/forms";
+import { Router } from "@angular/router";
 import { Heroe } from "../../interfaces/heroe.interface";
+import { HeroesService } from "../../services/heroes.service";
 
 @Component({
   selector: 'app-heroe',
@@ -15,13 +17,19 @@ export class HeroeComponent implements OnInit {
     bio:""
   }
 
-  constructor() { }
+  constructor(private _heroesService:HeroesService, private _router:Router) { }
 
   ngOnInit() {
   }
 
   guardar(){
     console.log(this.heroe);
+    //this._heroesService.nuevoHeroe(this.heroe); // si lo dejo así no se hará hasta que no nos subscribamos
+    this._heroesService.nuevoHeroe(this.heroe).subscribe( data => {
+        this._router.navigate(['/heroe', data.name])
+    }, error => {
+      console.error(error)
+    })
   }
 
 }
--- seccion 11/heroesapp/src/app/services/heroes.service.ts
+++ seccion 11/heroesapp/src/app/services/heroes.service.ts
@@ -1,8 +1,30 @@
 import { Injectable } from '@angular/core';
+import { Http, Headers } from "@angular/http";
+import "rxjs/Rx";
+
+//interface
+import { Heroe } from "../interfaces/heroe.interface";
 
 @Injectable()
 export class HeroesService {
 
-  constructor() { }
+  heroesURL:string = "https://heroesapp-9a17c.firebaseio.com/heroes.json"
+
+  constructor(private _http:Http) {
+
+   }
+
+   nuevoHeroe(heroe:Heroe){
+     let body = JSON.stringify(heroe); // genera un string de un json válido
+     let headers = new Headers({
+       'Content-type':'application/json'
+     }); 
+
+     return this._http.post( this.heroesURL, body, { headers })  // esto { headers:headers } es igual a esto { headers} en ES6, si se llaman igual se pueden omitir
+          .map( res=> {
+            console.log(res.json());
+            return res.json()
+          });
+   }
 
 }
