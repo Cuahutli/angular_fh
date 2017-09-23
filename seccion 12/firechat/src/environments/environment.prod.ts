204487fff56b9466037d39d6e7b2e32af5bba40b
Cuahutli Miguel Ulloa Robles <ing.cuahutliulloa@gmail.com>
2017-09-22 18:22:46 -0600

    listo firechat para github pages

--- seccion 12/firechat/src/environments/environment.prod.ts
+++ seccion 12/firechat/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCLbZ7zsBj1xeQ4yjMQXMuBg-q3K_jxxbg",
+    authDomain: "firechat-c4277.firebaseapp.com",
+    databaseURL: "https://firechat-c4277.firebaseio.com",
+    projectId: "firechat-c4277",
+    storageBucket: "firechat-c4277.appspot.com",
+    messagingSenderId: "373935784696"
+  }
 };
