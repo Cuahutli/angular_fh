5999972b16fa71da8aca279d0dca17aa501b3541
Cuahutli Miguel Ulloa Robles <ing.cuahutliulloa@gmail.com>
2017-09-27 11:47:15 -0600

    creando el proyecto de peliculas app y la clase 196 para usar jsonp en crossdomain

---
+++ seccion 13/peliculas/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpModule, JsonpModule } from "@angular/http";
+
+import { PeliculasService } from "./services/peliculas.service";
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpModule,
+    JsonpModule,
+  ],
+  providers: [ PeliculasService ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ seccion 13/peliculas/src/app/services/peliculas.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+
+import { Http, Jsonp } from "@angular/http";
+import "rxjs/Rx"; //para utilizar el map
+
+@Injectable()
+export class PeliculasService {
+
+  private apikey:string = "adb57d3308c2c5633486f667b0b2ca65";
+  private urlMoviedb:string = "https://api.themoviedb.org/3";
+
+  constructor(  private _jsonp:Jsonp, 
+                private _http:Http) {
+  }
+
+  getPopulares(){
+    let url = `${this.urlMoviedb}/discover/movie?sort_by=popularity.desc&api_key=${this.apikey}&language=es&callback=JSONP_CALLBACK`;
+
+    return this._jsonp.get( url ).map( res=> res.json());
+  }
+
+}
