3b783cef95e688586900edd208795909c8d1ca71
Cuahutli Miguel Ulloa Robles <ing.cuahutliulloa@gmail.com>
2017-12-11 14:02:58 -0700

    versión anterior seccion 13 (hasta aqui quedó y la iniciamos de nuevo)

--- seccion 13/peliculas/src/app/app.module.ts
+++ seccion 13/peliculas/src/app/app.module.ts
@@ -2,6 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule, JsonpModule } from "@angular/http";
 
+// constante rutas
+import { APP_ROUTING } from "./app.routes";
+
+
+//componentes
 import { PeliculasService } from "./services/peliculas.service";
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
@@ -21,6 +26,7 @@ import { DetalleComponent } from './components/detalle/detalle.component';
     BrowserModule,
     HttpModule,
     JsonpModule,
+    APP_ROUTING
   ],
   providers: [ PeliculasService ],
   bootstrap: [AppComponent]
