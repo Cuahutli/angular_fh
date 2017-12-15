6de4d965ec8fa4e11222afe230d10d209a48778b
Cuahutli Miguel Ulloa Robles <ing.cuahutliulloa@gmail.com>
2017-12-14 23:03:51 -0700

    208 Pantalla de peliculas

--- seccion 13_new/pelisApp/src/app/components/pelicula/pelicula.component.ts
+++ seccion 13_new/pelisApp/src/app/components/pelicula/pelicula.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
+import { PeliculasService } from "../../services/peliculas.service";
 
 @Component({
   selector: 'app-pelicula',
@@ -7,7 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PeliculaComponent implements OnInit {
 
-  constructor() { }
+  pelicula:any;
+
+  constructor(public _ps:PeliculasService, public route:ActivatedRoute) {
+      this.route.params.subscribe(params=>{
+        console.log(params);
+        this._ps.getPelicula(params['id']).subscribe(data=>{
+          this.pelicula=data;
+          console.log(this.pelicula)
+        });
+      })
+   }
 
   ngOnInit() {
   }
