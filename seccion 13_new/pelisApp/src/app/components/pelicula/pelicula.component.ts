a56b8f7037b79db4c431cce06af61433d2bd1cb2
Cuahutli Miguel Ulloa Robles <ing.cuahutliulloa@gmail.com>
2017-12-14 23:30:11 -0700

    209 Detalles finales de la aplicación de peliculas

--- seccion 13_new/pelisApp/src/app/components/pelicula/pelicula.component.ts
+++ seccion 13_new/pelisApp/src/app/components/pelicula/pelicula.component.ts
@@ -10,10 +10,15 @@ import { PeliculasService } from "../../services/peliculas.service";
 export class PeliculaComponent implements OnInit {
 
   pelicula:any;
+  regresarA:string=""
+  busqueda:string=""
 
   constructor(public _ps:PeliculasService, public route:ActivatedRoute) {
       this.route.params.subscribe(params=>{
-        console.log(params);
+        this.regresarA = params['pag'];
+        if(params['busqueda']){
+          this.busqueda = params['busqueda'];
+        }
         this._ps.getPelicula(params['id']).subscribe(data=>{
           this.pelicula=data;
           console.log(this.pelicula)
