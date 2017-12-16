fa1f11723be153633fb61df885c3c34bd2a6ca08
Cuahutli Miguel Ulloa Robles <ing.cuahutliulloa@gmail.com>
2017-12-16 13:13:28 -0700

    2016 Eventos en el Mapa - Crear marcadores, arrastrarlos y obtener coordenadas

--- seccion 14/mapas/src/app/app.component.ts
+++ seccion 14/mapas/src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+import { MapasService } from "./services/mapas.service";
+import { Marcador } from "./interfaces/marcador";
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -10,4 +13,18 @@ export class AppComponent {
   lat:number = 21.473311;
   lng:number =  -104.855755;
   zoom:number= 15;
+
+  constructor(public _ms:MapasService){
+
+  }
+  clickMapa(event){
+    console.log(event)
+    let nuevo_marcador:Marcador = {
+      lat:event.coords.lat,
+      lng: event.coords.lng,
+      titulo: "Sin titulo",
+      draggable: true
+    }
+    this._ms.insertarMarcador(nuevo_marcador)
+  }
 }
--- seccion 14/mapas/src/app/services/mapas.service.ts
+++ seccion 14/mapas/src/app/services/mapas.service.ts
@@ -1,8 +1,23 @@
 import { Injectable } from '@angular/core';
+import { Marcador } from "../interfaces/marcador";
 
 @Injectable()
 export class MapasService {
+  marcadores:Marcador[] = [];
+  constructor() { 
+    let nuevo_marcador:Marcador = {
+      lat:21.473311,
+      lng:-104.855755,
+      titulo:"Procuraduria",
+      draggable:true,
+      desc: "esta es la procu"
+    }
 
-  constructor() { }
+    this.marcadores.push(nuevo_marcador)
+  }
+
+  insertarMarcador(marcador:Marcador){
+    this.marcadores.push(marcador)
+  }
 
 }
